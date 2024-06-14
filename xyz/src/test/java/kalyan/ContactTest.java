c1a346021ad61185b093d5c50db0ffee0f2277ca
Kalyan0832 <spk9640@gmail.com>
2024-06-14 13:30:53 +0530

    xyz

---
+++ xyz/src/test/java/kalyan/ContactTest.java
@@ -0,0 +1,19 @@
+package kalyan;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContact() {
+		System.out.println("create contact executed");
+	}
+	
+	@Test
+	public void modifyContact() {
+		System.out.println("modify contact executed");
+	}
+	@Test
+	public void deleteContact() {
+		System.out.println("delete contact executed");
+	}
+}
---
+++ xyz/src/test/java/kalyan/OrgTest.java
@@ -0,0 +1,19 @@
+package kalyan;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrg() {
+		System.out.println("create Org executed");
+	}
+	
+	@Test
+	public void modifyOrg() {
+		System.out.println("modify Org executed");
+	}
+	@Test
+	public void deleteOrg() {
+		System.out.println("delete Org executed");
+	}
+}
