0ce5e8ca8139b2397f4416c30bc50edc11a27bfd
Kalyan0832 <spk9640@gmail.com>
2024-06-15 10:42:33 +0530

    changes added

--- xyz/src/test/java/kalyan/ContactTest.java
+++ xyz/src/test/java/kalyan/ContactTest.java
@@ -2,28 +2,56 @@ package kalyan;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ContactTest {
 	@Test
 	public void createContact() {
 		System.out.println("create contact executed");
 	}
-	
+
 	@Test
 	public void modifyContact() {
 		System.out.println("modify contact executed");
 	}
+
 	@Test
 	public void deleteContact() {
 		System.out.println("delete contact executed");
 	}
-	@Test
-	public void m1() throws InterruptedException {
-		WebDriver driver=new ChromeDriver();
-		driver.get("http://localhost:8888/");
-		Thread.sleep(5000);
-		System.out.println("from m1()");
-		driver.quit();
+
+	@BeforeClass
+	public void beforeClass() throws InterruptedException {
+		String browser = System.getProperty("browser");
+
+//		WebDriver driver = null;
+//		if (browser.equalsIgnoreCase("chrome")) {
+//			WebDriverManager.chromedriver().setup();
+//			driver = new ChromeDriver();
+//		} else if (browser.equalsIgnoreCase("edge")) {
+//			WebDriverManager.edgedriver().setup();
+//			driver = new EdgeDriver();
+//		} else if (browser.equalsIgnoreCase("firefox")) {
+//			WebDriverManager.firefoxdriver().setup();
+//			driver = new FirefoxDriver();
+//		}
+		
+//		driver.get("http://localhost:8888/");
+		Thread.sleep(3000);
+		
+//		driver.quit();
+		
+		String env = System.getProperty("envi", "http://localhost:8888/");
+		System.out.println(env);
+		System.out.println("launch "+browser);
+		String user = System.getProperty("username", "admin");
+		System.out.println(user);
+		String password = System.getProperty("password", "root");
+		System.out.println(password);
 	}
 }
